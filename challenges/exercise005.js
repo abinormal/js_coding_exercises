f9e8077516c83747ed4a8519e5b4e7d35cb9e904
Abi Sidford <asidford@gmail.com>
2020-09-06 16:04:52 +0100

    Implemented sumArrays()

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -22,17 +22,52 @@ const findNextNumber = (nums, n) => {
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+  //Count 1s and 0s - return result in object
+
+  var count = {
+    0: 0,
+    1: 0
+  };
+
+  for (var i=0; i<str.length ; i++){
+    if (str.substring(i,i+1) == 1 ){
+      count[1] += 1;
+    } else {
+      count[0] += 1;
+    }
+  }
+  return count;
 };
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  
+  var z = n;
+  var reverse = 0;
+
+  while(z > 0)
+  {
+      var digit = z % 10;
+      reverse = (reverse * 10) + digit;
+      z = parseInt(z / 10);
+  }
+  return reverse;
+
 };
 
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
+  
+  var total = 0;
+
+  for (var i=0; i<arrs.length;i++){
+    for (var j=0; j<arrs[i].length;j++){
+      total += arrs[i].[j];
+
+    }
+
+  }
+  return total;
 };
 
 const arrShift = arr => {
