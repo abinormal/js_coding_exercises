80a0b2027094ebfed938fd9f162d6a7d25315b4c
Abi Sidford <asidford@gmail.com>
2020-10-07 23:09:14 +0100

    Slight refactoring and tidy up

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -1,81 +1,81 @@
 function capitalize(word) {
-  if (word === undefined) throw new Error("word is required");
-  return word.substring(0,1).toUpperCase() + word.substring(1,word.length);
+  if (word === undefined) throw new Error('word is required');
+  return word.substring(0, 1).toUpperCase() + word.substring(1, word.length);
 }
 
 function generateInitials(firstName, lastName) {
-  if (firstName === undefined) throw new Error("firstName is required");
-  if (lastName === undefined) throw new Error("lastName is required");
-  return firstName.substring(0,1) + '.' + lastName.substring(0,1);
+  if (firstName === undefined) throw new Error('firstName is required');
+  if (lastName === undefined) throw new Error('lastName is required');
+  return firstName.substring(0, 1) + '.' + lastName.substring(0, 1);
 }
 
 function addVAT(originalPrice, vatRate) {
-  if (originalPrice === undefined) throw new Error("originalPrice is requied");
-  if (vatRate === undefined) throw new Error("vatRate is required");
-  
-  let total = originalPrice + ((originalPrice/100)*vatRate);
+  if (originalPrice === undefined) throw new Error('originalPrice is requied');
+  if (vatRate === undefined) throw new Error('vatRate is required');
+
+  let total = originalPrice + (originalPrice / 100) * vatRate;
 
   // push string back into a number as test complains about quotation marks
-  return parseFloat(total.toFixed(2));  
+  return parseFloat(total.toFixed(2));
 }
 
 function getSalePrice(originalPrice, reduction) {
-  if (originalPrice === undefined) throw new Error("originalPrice is required");
-  if (reduction === undefined) throw new Error("reduction is required");
- 
-  let newPrice = originalPrice - ((originalPrice/100)*reduction);
+  if (originalPrice === undefined) throw new Error('originalPrice is required');
+  if (reduction === undefined) throw new Error('reduction is required');
+
+  let newPrice = originalPrice - (originalPrice / 100) * reduction;
   return parseFloat(newPrice.toFixed(2));
 }
 
 function getMiddleCharacter(str) {
-  if (str === undefined) throw new Error("str is required");
+  if (str === undefined) throw new Error('str is required');
 
   //Get string length and Find the middle value
-  let middle = (str.length)/2;
+  let middle = str.length / 2;
 
   //find even and odd
-  if (Number.isInteger(middle) ) {
+  if (Number.isInteger(middle)) {
     // if even give two middle letters
-     return str.substring(middle-1,middle+1);
+    return str.substring(middle - 1, middle + 1);
   } else {
     //if odd give single middle letter
-    return str.substring(middle,middle+1);
+    return str.substring(middle, middle + 1);
   }
 }
 
 function reverseWord(word) {
-  if (word === undefined) throw new Error("word is required");
-  return word.split("").reverse().join("");
+  if (word === undefined) throw new Error('word is required');
+  return word.split('').reverse().join('');
 }
 
 function reverseAllWords(words) {
-  if (words === undefined) throw new Error("words is required");
-  
+  if (words === undefined) throw new Error('words is required');
+
   let newWords = [];
   // forEach word in words, reverse and add to new array.
-  words.forEach((word)=>
-  {
-    newWords.push(word.split("").reverse().join(""));
-  })
+  words.forEach(word => {
+    newWords.push(word.split('').reverse().join(''));
+  });
   return newWords;
 }
 
 function countLinuxUsers(users) {
-  if (users === undefined) throw new Error("users is required");
+  if (users === undefined) throw new Error('users is required');
   let linuxUsers = 0;
   //Get each one, check for type = "linux"
-  users.forEach((user)=>{user.type == "Linux" ? linuxUsers++ : null})
+  users.forEach(user => {
+    user.type == 'Linux' ? linuxUsers++ : null;
+  });
 
   return linuxUsers;
 }
 
 function getMeanScore(scores) {
-  if (scores === undefined) throw new Error("scores is required");
-  let total = 0; 
+  if (scores === undefined) throw new Error('scores is required');
+  let total = 0;
 
-  //Add all the scores.
-  scores.forEach((score)=>{total+=score})
- 
+  //Add all the scores and get avg
+  total += scores.reduce((a, b) => (a += b));
   let average = total / scores.length;
 
   //truncate the trailing values
@@ -83,26 +83,15 @@ function getMeanScore(scores) {
 }
 
 function simpleFizzBuzz(n) {
-  if (n === undefined) throw new Error("n is required");
-  let fizz = false;
-  let buzz = false;
+  if (n === undefined) throw new Error('n is required');
+
+  // Number divisible by 3 and 5 will always divide by 15
+  if (n % 15 == 0) return 'fizzbuzz';
   //if it can be divided by 3 - fizz
-  if (Number.isInteger(n/3))
-    fizz = true;
-
+  if (n % 3 == 0) return 'fizz';
   //if it can be divided by 5 - buzz
-  if (Number.isInteger(n/5))
-    buzz = true;
-
-  //both - fizzbuzz    
-  if (fizz & buzz)
-    return "fizzbuzz";  
-  if (fizz)
-    return "fizz";
-  if (buzz)
-    return "buzz";
-  
-  //neither - return number
+  if (n % 5 == 0) return 'buzz';
+  // otherwise return number
   return n;
 }
 
@@ -116,5 +105,5 @@ module.exports = {
   reverseAllWords,
   countLinuxUsers,
   getMeanScore,
-  simpleFizzBuzz
+  simpleFizzBuzz,
 };
--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -1,73 +1,74 @@
 function getSquares(nums) {
-  if (nums === undefined) throw new Error("nums is required");
+  if (nums === undefined) throw new Error('nums is required');
   // parse array, get square of each number, save to same array location
 
   let newNums = [];
-  nums.forEach(num=>newNums.push(num*num))
+  nums.forEach(num => newNums.push(num * num));
 
   return newNums;
 }
 
 function camelCaseWords(words) {
-  if (words === undefined) throw new Error("words is required");
+  if (words === undefined) throw new Error('words is required');
   //for loop skipping the first entry
   let camCase = words[0];
-  for (let i = 1 ; i<words.length ; i++) {
-    camCase += words[i].substring(0,1).toUpperCase() + words[i].substring(1,words[i].length);
+  for (let i = 1; i < words.length; i++) {
+    camCase +=
+      words[i].substring(0, 1).toUpperCase() +
+      words[i].substring(1, words[i].length);
   }
   return camCase;
 }
 
 function getTotalSubjects(people) {
-  if (people === undefined) throw new Error("people is required");
+  if (people === undefined) throw new Error('people is required');
   let iSubjects = 0;
   // for each person count how many entries in subjects arr
-  for (let i = 0; i<people.length ; i++){
+  for (let i = 0; i < people.length; i++) {
     //console.log("Subjects: " + people[i].subjects);
-    for (let j = 0 ; j<people[i].subjects.length ; j++) {
+    for (let j = 0; j < people[i].subjects.length; j++) {
       //if array not empty then ++
       iSubjects++;
     }
   }
-return iSubjects;
-//console.log("Subjects: " + iSubjects);
+  return iSubjects;
+  //console.log("Subjects: " + iSubjects);
 }
 
 function checkIngredients(menu, ingredient) {
-  if (menu === undefined) throw new Error("menu is required");
-  if (!ingredient) throw new Error("ingredient is required");
+  if (menu === undefined) throw new Error('menu is required');
+  if (!ingredient) throw new Error('ingredient is required');
   // given menu (array) check for ingredient
   let foundIt = false;
 
-  for (let i = 0 ; i<menu.length ; i++){
-    for (let j = 0 ; j<menu[i].ingredients.length ; j++) {
-      if (menu[i].ingredients[j] == ingredient)
-        foundIt = true;
+  for (let i = 0; i < menu.length; i++) {
+    for (let j = 0; j < menu[i].ingredients.length; j++) {
+      if (menu[i].ingredients[j] == ingredient) foundIt = true;
     }
   }
   return foundIt;
 }
 
 function duplicateNumbers(arr1, arr2) {
-  if (arr1 === undefined) throw new Error("arr1 is required");
-  if (arr2 === undefined) throw new Error("arr2 is required");
+  if (arr1 === undefined) throw new Error('arr1 is required');
+  if (arr2 === undefined) throw new Error('arr2 is required');
   // Return array of duplicate numbers found in an array
 
   let aDuplicates = [];
   let iDupCount = 0;
 
-  for (let i = 0 ; i <arr1.length ; i++){
-    for (let j = 0 ; j <arr2.length ; j++){
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
       // check for duplicate
-      if (arr1[i]==arr2[j]){
+      if (arr1[i] == arr2[j]) {
         // Found - Add to array
         aDuplicates[iDupCount] = arr1[i];
         iDupCount++;
-      }  
+      }
     }
   }
   // Remove duplicates and sort
-  let unique = [...new Set(aDuplicates)]; 
+  let unique = [...new Set(aDuplicates)];
   return unique.sort();
 }
 
@@ -76,5 +77,5 @@ module.exports = {
   camelCaseWords,
   getTotalSubjects,
   checkIngredients,
-  duplicateNumbers
+  duplicateNumbers,
 };
